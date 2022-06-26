9266c9653d430fd4e4d4617ecd25e9350f4a8a43
Nadir Payam <nadirpayam@hotmail.com>
2022-06-26 15:13:17 +0300

    Java Dersleri başladı.

---
+++ src/DegiskenlerVeriTipleri/DegiskenMantigi.java
@@ -0,0 +1,22 @@
+package DegiskenlerVeriTipleri;
+
+public class DegiskenMantigi {
+
+	public static void main(String[] args) {
+
+          int ogrencininNotu; // deðiþkenler adlarýný camel case mantýðýyla yazýyoruz ilk har küçük sonrasý büyük
+           
+          ogrencininNotu = 90; 
+          
+          String mesaj = "Öðrencinin notu: "; 
+
+          
+          System.out.println(mesaj + ogrencininNotu);
+
+	}
+
+}
+
+// Java büyük küçük harf duyarlýdýr.
+
+//reusability tekrar kullanýlabilirliktir yani bir kere yaz onu diðer yerlerde de kullan, ayný þeyi defalarca yazma
---
+++ src/DegiskenlerVeriTipleri/Output.java
@@ -0,0 +1,14 @@
+package DegiskenlerVeriTipleri;
+
+public class Output {
+
+	public static void main(String[] args) {
+
+		 System.out.println("Hello Java!"); // Almak istediðimiz çýktýyý parantez içine yazýyoruz. 
+
+	}
+
+}
+
+
+//intellisense programcýya kodu yazarken yardýmcý görevi görür örnekler sunar, ide yapýyor bu iþi.
